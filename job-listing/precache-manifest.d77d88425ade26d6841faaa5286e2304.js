9962ff37f1ede70870b2cc912c8883588ba614e8
Ahmed Goda <ahmedmosad405@gmail.com>
2020-08-20 14:36:29 +0200

    Updates

---
+++ job-listing/precache-manifest.d77d88425ade26d6841faaa5286e2304.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963bb0286cfa8fadaad16db38bbbfc8a",
+    "url": "/job-listing/index.html"
+  },
+  {
+    "revision": "e28665c0d94c9388dda7",
+    "url": "/job-listing/static/css/2.67fa2020.chunk.css"
+  },
+  {
+    "revision": "db7d9d43033c63fa1f63",
+    "url": "/job-listing/static/css/main.866673cc.chunk.css"
+  },
+  {
+    "revision": "e28665c0d94c9388dda7",
+    "url": "/job-listing/static/js/2.e9dd0837.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/job-listing/static/js/2.e9dd0837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7d9d43033c63fa1f63",
+    "url": "/job-listing/static/js/main.911aad6c.chunk.js"
+  },
+  {
+    "revision": "cb84270f42c17b7bdfb8",
+    "url": "/job-listing/static/js/runtime-main.3f5338fb.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/job-listing/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/job-listing/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/job-listing/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/job-listing/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/job-listing/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/job-listing/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/job-listing/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/job-listing/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/job-listing/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/job-listing/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/job-listing/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/job-listing/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/job-listing/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/job-listing/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/job-listing/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
--- job-listing/service-worker.js
+++ job-listing/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.642eef6b4cb6d045f0dde2230648b410.js"
+  "/job-listing/precache-manifest.d77d88425ade26d6841faaa5286e2304.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/job-listing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
