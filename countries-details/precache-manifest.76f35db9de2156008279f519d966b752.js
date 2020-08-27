5c841a973c1ee917ac3b883661440cb2fdff558b
Ahmed Goda <ahmedmosad405@gmail.com>
2020-08-27 11:54:10 +0200

    Updates

---
+++ countries-details/precache-manifest.76f35db9de2156008279f519d966b752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf8ba234ed988647a622466fdeea3fe",
+    "url": "/countries-details/index.html"
+  },
+  {
+    "revision": "c66c3aeabadacd633232",
+    "url": "/countries-details/static/css/main.c6dcad96.chunk.css"
+  },
+  {
+    "revision": "4bd04037c59af6fa9f63",
+    "url": "/countries-details/static/js/2.643e289c.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/countries-details/static/js/2.643e289c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66c3aeabadacd633232",
+    "url": "/countries-details/static/js/main.e9777660.chunk.js"
+  },
+  {
+    "revision": "0a4670855e6a7b5ab42e",
+    "url": "/countries-details/static/js/runtime-main.576163c1.js"
+  }
+]);
